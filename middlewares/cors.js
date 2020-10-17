96b0d15de2c7f06fa6aa5b019f9762a3f3d8f191
Natalya <NatalyaArt2019@yandex.ru>
2020-10-18 00:00:28 +0300

    cors policy

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,20 +1,5 @@
-const allowedCors = [
-  'https://api.news-explorer-yp.tk',
-  'http://api.news-explorer-yp.tk',
-  'https://news-explorer-yp.tk',
-  'http://news-explorer-yp.tk',
-  'http://localhost:8080',
-  'https://localhost:8080',
-  'http://localhost',
-  'https://localhost',
-];
-
 const cors = (err, req, res, next) => {
-  const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
-
-  if (allowedCors.includes(origin)) { // Проверяем, что значение origin есть в разрешённых доменах
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
 
   next();
 };
