f921ba854d4c3df4a368b159cdb1791ae1136f38
Natalya <NatalyaArt2019@yandex.ru>
2020-10-18 00:14:07 +0300

    cors policy

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(limiter);
 app.use(helmet());
-app.use(cors());
+//app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
