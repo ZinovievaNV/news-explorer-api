56ce2f7eac125670ba85d8195da1c7fc2aff358e
Natalya <NatalyaArt2019@yandex.ru>
2020-10-18 21:58:43 +0300

    cors policy

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,7 @@
 const cors = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
   next();
 };
